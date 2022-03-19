36e05c050393115bc3dd643339e61b21fa9af173
Viet <viet.dang@dirox.net>
2022-03-20 00:31:30 +0700

    Adding Coach interface and make BaseballCoach implement it

--- src/com/luv2code/springdemo/BaseballCoach.java
+++ src/com/luv2code/springdemo/BaseballCoach.java
@@ -1,7 +1,10 @@
 package com.luv2code.springdemo;
 
-public class BaseballCoach {
+public class BaseballCoach implements Coach {
+
+	@Override
 	public String getDailyWorkout() {
+		// TODO Auto-generated method stub
 		return "spend 30 minutes on batching practice";
 	}
 }
