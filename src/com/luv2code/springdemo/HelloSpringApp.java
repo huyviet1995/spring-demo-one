f3516c7e640e157777d0ac830eab3f139c02aef9
Viet <viet.dang@dirox.net>
2022-03-20 23:33:39 +0700

    Adding SoccerCoach bean

--- src/com/luv2code/springdemo/HelloSpringApp.java
+++ src/com/luv2code/springdemo/HelloSpringApp.java
@@ -14,9 +14,11 @@ public class HelloSpringApp {
 		
 		// retrieve the bean from the spring container
 		Coach theCoach = context.getBean("myCoach", Coach.class);
+		Coach theSoccerCoach = context.getBean("myCoach1", Coach.class);
 		
 		// call methods on the beans
 		System.out.println(theCoach.getDailyWorkout());
+		System.out.println(theSoccerCoach.getDailyWorkout());
 		
 		// close the context
 		context.close();
---
+++ src/com/luv2code/springdemo/SoccerCoach.java
@@ -0,0 +1,10 @@
+package com.luv2code.springdemo;
+
+public class SoccerCoach implements Coach {
+
+	@Override
+	public String getDailyWorkout() {
+		return "Run around the stadium 5 times";
+	}
+
+}
