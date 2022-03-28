6e9c38c6228dbad0720ca34f7d1e501d5fb260bb
Viet <viet.dang@dirox.net>
2022-03-28 18:27:44 +0700

    0 Adding the FortuneService 1. Define the beans 2. Adding the constructor 3. Call the function inside the ApplicationContext.

--- src/com/luv2code/springdemo/BaseballCoach.java
+++ src/com/luv2code/springdemo/BaseballCoach.java
@@ -1,10 +1,24 @@
 package com.luv2code.springdemo;
 
 public class BaseballCoach implements Coach {
+	
+	// defind a private field for dependency
+	private FortuneService fortuneService;
+	
+	// Define a constructor for dependencies injection
+	public BaseballCoach(FortuneService theFortuneService) {
+		fortuneService = theFortuneService;
+	}
 
 	@Override
 	public String getDailyWorkout() {
-		// TODO Auto-generated method stub
+		// use my fortune service to get the fortune service
 		return "spend 30 minutes on batching practice";
+	}
+
+	@Override
+	public String getDailyFortune() {
+		// TODO Auto-generated method stub
+		return fortuneService.getFortune();
 	}
 }
--- src/com/luv2code/springdemo/HelloSpringApp.java
+++ src/com/luv2code/springdemo/HelloSpringApp.java
@@ -14,13 +14,13 @@ public class HelloSpringApp {
 		
 		// retrieve the bean from the spring container
 		Coach theCoach = context.getBean("myCoach", Coach.class);
-		Coach theSoccerCoach = context.getBean("myCoach1", Coach.class);
 		
 		// call methods on the beans
 		System.out.println(theCoach.getDailyWorkout());
-		System.out.println(theSoccerCoach.getDailyWorkout());
 		
 		// close the context
+		System.out.println(theCoach.getDailyFortune());
+
 		context.close();
 	}
 
