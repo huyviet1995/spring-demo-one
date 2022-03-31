4839f867c4e2fa73386bcc0562d37b14c57a135b
Viet Dang <viet.dang@dirox.net>
2022-03-31 12:03:23 -0400

    Create the CricketCoach

--- src/com/luv2code/springdemo/CricketCoach.java
+++ src/com/luv2code/springdemo/CricketCoach.java
@@ -8,6 +8,14 @@ public class CricketCoach implements Coach {
 		System.out.println("Cricket Coach: Inside No Arg Constructor");
 	}
 	
+	public FortuneService getFortuneService() {
+		return fortuneService;
+	}
+
+	public void setFortuneService(FortuneService fortuneService) {
+		this.fortuneService = fortuneService;
+	}
+
 	@Override
 	public String getDailyWorkout() {
 		return "Practice fast bowling for 15 minutes";
