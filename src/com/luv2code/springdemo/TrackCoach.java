f4c8bc1710761ad896a3f4159eaa5accba9c1235
Viet Dang <viet.dang@dirox.net>
2022-03-31 11:47:06 -0400

    Create the getDailyWorkout in TrackCoach

--- src/com/luv2code/springdemo/TrackCoach.java
+++ src/com/luv2code/springdemo/TrackCoach.java
@@ -1,16 +1,20 @@
 package com.luv2code.springdemo;
 
 public class TrackCoach implements Coach {
+	
+	private FortuneService fortuneService;
+	
+	public TrackCoach() {};
+
+	public TrackCoach(FortuneService fortuneService) {
+		super();
+		this.fortuneService = fortuneService;
+	}
 
 	@Override
 	public String getDailyWorkout() {
-		return "Run a hard 5k";
+		return "Just do it: " + fortuneService.getFortune();
 	}
 
-	@Override
-	public String getDailyFortune() {
-		// TODO Auto-generated method stub
-		return null;
-	}
 
 }
