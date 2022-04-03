ec741efba754f57991a31bef25fd073c4c569bf3
Viet Dang <viet.dang@dirox.net>
2022-04-03 14:41:01 +0700

    Use Component Scan to create bean
    Steps:
    1. Load the config file
    2. Get the bean from the spring container
    3. Call method on the bean

---
+++ src/com/luv2code/springdemo/AnnotationDemoApp.java
@@ -0,0 +1,19 @@
+package com.luv2code.springdemo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class AnnotationDemoApp {
+	public static void main(String[] args) {
+		// read string config file
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+		
+		// get the bean from the spring container
+		Coach theCoach = context.getBean("thatSillyCoach", TennisCoach.class);
+		
+		// Call a method on the bean
+		System.out.println(theCoach.getDailyWorkout());
+		
+		// Close the container
+		context.close();
+	}
+}
