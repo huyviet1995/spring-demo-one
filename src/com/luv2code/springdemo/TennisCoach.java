dda2ae2deaa1eaf30cdde6480b6d98db24e812d4
Viet Dang <viet.dang@dirox.net>
2022-04-04 22:51:57 +0700

    Adding more Services and set qualifier
    Set qualifier to actually decide which bean of type FortuneService should be chosen to be injected into the TennisCoach bean

--- src/com/luv2code/springdemo/TennisCoach.java
+++ src/com/luv2code/springdemo/TennisCoach.java
@@ -1,16 +1,36 @@
 package com.luv2code.springdemo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class TennisCoach implements Coach {
-
-	private FortuneService fortuneService;
 	
 	@Autowired
-	public TennisCoach(FortuneService theFortuneService) {
+	@Qualifier("happyFortuneService")
+	private FortuneService fortuneService;
+
+	// Define the default constructor
+	public TennisCoach() {
+		System.out.println("TennisCoach: Inside default constructor");
+	}
+
+	
+	@Autowired
+	public TennisCoach(@Qualifier("happyFortuneService") FortuneService theFortuneService) {
+		System.out.println("Tennic Coach: Inside the main constructor");
 		this.fortuneService = theFortuneService;
+	}
+
+	public FortuneService getFortuneService() {
+		System.out.println("TennisCoach: Inside get fortune service");
+		return fortuneService;
+	}
+
+	@Autowired
+	public void setFortuneService(@Qualifier("happyFortuneService") FortuneService fortuneService) {
+		this.fortuneService = fortuneService;
 	}
 
 	@Override
@@ -20,7 +40,6 @@ public class TennisCoach implements Coach {
 
 	@Override
 	public String getDailyFortune() {
-		// TODO Auto-generated method stub
 		return fortuneService.getFortune();
 	}
 
